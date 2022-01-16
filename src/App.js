c67aa6da97a5e48be92e70c9bd2a185d339baf67
AdeBintang19 <97815845+AdeBintang19@users.noreply.github.com>
2022-01-16 14:57:24 +0700

    jenuh

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navbar">header</div>
+
+      <div className="card section">1</div>
+      <div className="card section">2</div>
+      <div className="card section">3</div>
+      <div className="card section">4</div>
+      <div className="card section">5</div>
+      <div className="card section">6</div>
+
+      <div className="body">
+        <div className="body-anak body-1"></div>
+        <div className="body-anak body-2"></div>
+        <div className="body-anak body-3"></div>
+      </div>
+    </div>
+  );
+}
+export default App;
